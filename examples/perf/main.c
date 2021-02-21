6262baf85b34a226b780161920b4863fc90253e5
Gabriel Marcano <gabemarcano@yahoo.com>
2021-02-21 00:58:52 -0800

    Added perf example
     - Example using new perf capsule

---
+++ examples/perf/main.c
@@ -0,0 +1,14 @@
+/* vim: set sw=2 expandtab tw=80: */
+
+#include <stdio.h>
+#include <perf.h>
+
+int main(void) {
+  unsigned count = perf_count();
+  unsigned cycles1 = perf_cycles();
+  unsigned cycles2 = perf_cycles();
+  printf("Perf available: %u\n", count);
+  printf("Perf1: %u, Perf2: %u, diff: %u\n",
+    cycles1, cycles2, cycles2 - cycles1);
+  return 0;
+}
