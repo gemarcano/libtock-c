60b9a7b14ad5c7daa207ff0b3af7b504ea969c51
Gabriel Marcano <gabemarcano@yahoo.com>
2021-02-21 21:06:53 -0800

    Update Perf return types
     - Neither function should ever fail, and thus they return unsigned
       values.

--- libtock/perf.c
+++ libtock/perf.c
@@ -4,10 +4,10 @@ int perf_count(void) {
   return command(DRIVER_PERF, 0, 0, 0);
 }
 
-int perf_cycles(void) {
-  return command(DRIVER_PERF, 1, 0, 0);
+unsigned perf_cycles(void) {
+  return (unsigned)command(DRIVER_PERF, 1, 0, 0);
 }
 
-int perf_instructions_retired(void) {
-  return command(DRIVER_PERF, 2, 0, 0);
+unsigned perf_instructions_retired(void) {
+  return (unsigned)command(DRIVER_PERF, 2, 0, 0);
 }
--- libtock/perf.h
+++ libtock/perf.h
@@ -9,8 +9,8 @@ extern "C" {
 #define DRIVER_PERF 0x00090004
 
 int perf_count(void);
-int perf_cycles(void);
-int perf_instructions_retired(void);
+unsigned perf_cycles(void);
+unsigned perf_instructions_retired(void);
 
 #ifdef __cplusplus
 }
